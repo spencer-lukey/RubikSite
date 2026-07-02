c22cf55d8fde09bd45b5a40802d88d9b74dc549a
spencer-lukey <slukey@ualberta.ca>
2026-07-01 18:25:38 -0600

    Starting fresh

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import LoginPage from './pages/LoginPage'
+import 
 
 
 function App() {
 
-  return <LoginPage />;
+  return;
     
 }
 
