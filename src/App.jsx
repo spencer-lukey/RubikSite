b100a52ada8cdf4f547507820394b70c8da7724e
spencer-lukey <slukey@ualberta.ca>
2026-07-06 23:47:55 -0600

    Testing creating pieces from another file, adding in logic to display and group cubes to scene.

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,38 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Piece from './Piece.jsx'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { useRef } from 'react'
 import { OrbitControls, GizmoHelper, GizmoViewcube, GizmoViewport } from '@react-three/drei'
 import { backgroundBlurriness } from 'three/tsl'
 import { useControls } from 'leva'
 
+// GROUPING PIECES: https://r3f.docs.pmnd.rs/tutorials/how-it-works
+
 // DOCS: https://threejs.org/docs/
 // npm install three @react-three/fiber
 // npm install @react-three/drai
 // npm install leva@0.9.34
 
-function AnimationBox({ base, height, depth}) {
-
-  const boxRef = useRef();
-  
-  useFrame(() => {
-    // Runs repeatedly every frame!
-    
-  });
-
-  return (
-    <mesh ref={boxRef}>
-      <boxGeometry args={[base, height, depth]} />
-      <meshStandardMaterial color={0x00bfff} />
-    </mesh>
-  )
-}
-
 function App() {
+
+  // Create 27 piece objects in a cube shape of 3x3x3
+
+  var pieces = [];
+  var posY;
+  var posX;
+  var posZ;
+  for (posY = 1; posY >= -1; posY -= 1) {
+
+    // New layer
+    for (posZ = 1; posZ >= -1; posZ -= 1) {
+
+      for (posX = 1; posX >= -1; posX -= 1) {
+        
+      }
+    }
+  }
 
 
   // Returns main rendering for animation!
@@ -41,11 +44,12 @@ function App() {
         <GizmoHelper alignment='bottom-right' margin={[80, 80]}>
           <GizmoViewcube />
         </GizmoHelper>
-        <axesHelper args={[7]}/>
-        <gridHelper args={[20, 10, 0x0000ff, 0xff0000]}/>
+        <axesHelper args={[10]}/>
+        <gridHelper args={[20, 10, 0x222222, 0x222222]}/>
         <OrbitControls />
-        <AnimationBox />
-        <directionalLight position={[2, 5, 1]} />
+        <ambientLight intensity={Math.PI / 2} />
+        {pieces}
+        <Piece posX={0} posY={0} posZ={0} />
       </Canvas>
     </div>
   );
--- src/Piece.jsx
+++ src/Piece.jsx
@@ -1,6 +1,30 @@
-function Piece(props) {
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef, useState } from 'react'
+import { OrbitControls, GizmoHelper, GizmoViewcube, GizmoViewport } from '@react-three/drei'
+import { backgroundBlurriness } from 'three/tsl'
+import { useControls } from 'leva'
 
-    // Returns a cube given inputs
+
+function Piece({ posX, posY, posZ}) {
+
+    // Returns a cube given inputs on position
+    const boxRef = useRef();
+    const [isActive, setActive] = useState(false);
+
+    useFrame(() => {
+      // Runs repeatedly every frame!
+      
+    });
+
+    return (
+      <mesh 
+        ref={boxRef}
+        position={[posX, posY, posZ]}
+        onClick={(event) => (setActive(!isActive))}>
+        <boxGeometry args={[0.98, 0.98, 0.98]}/>
+        <meshStandardMaterial color={isActive ? 'orange' : 	0x222222} /> 
+      </mesh>
+    )
     
 
 
@@ -8,4 +32,6 @@ function Piece(props) {
 
 }
 
-export default Piece;
+
+
+export default Piece
