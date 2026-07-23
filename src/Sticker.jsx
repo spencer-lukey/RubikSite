dfa3b4f451873969de9e8ccfd681346e745ecd66
spencer-lukey <slukey@ualberta.ca>
2026-07-22 18:12:17 -0600

    work started on color for stickers

--- src/Sticker.jsx
+++ src/Sticker.jsx
@@ -4,15 +4,27 @@ import { OrbitControls, GizmoHelper, GizmoViewcube, GizmoViewport } from '@react
 import { abs, backgroundBlurriness } from 'three/tsl'
 import { useControls } from 'leva'
 
-
-
 function Sticker({ posX, posY, posZ, posFactor}) {
     // TODO: Given position of the piece, determine what dimensions the sticker should have
     // ...
+    function pickColor() {
+        const colors = ["red", "orange", "blue", "green", "white", "yellow"];
+        var idx = 0;
+        if (posFactor[0]) {
+            
+        } else if (posFactor[1]) {
+
+        } else if (posFactor[2]) {
+
+        }
+    }
+
+
+
     return (
-      <mesh position={[posX + (posX*posFactor[0]), posY + (posY*posFactor[1]), posZ + (posZ*posFactor[2])]}>
+      <mesh position={[posX*(1 + posFactor[0]), posY*(1 + posFactor[1]), posZ*(1 + posFactor[2])]}>
         <boxGeometry args={[0.9, 0.9, 0.9]}/>
-        <meshStandardMaterial color={{} ? 'orange' : 0x222222} /> 
+        <meshStandardMaterial color={pickColor()} /> 
       </mesh>
     )
 }
